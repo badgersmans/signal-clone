9ecb57bf29e4641f1e5b4f7fd0e7b5f5a2859363
Shawn Law <shawnlaw@tuta.io>
2022-12-01 06:42:25 +0800

    13.36.01 messages are working! fuck yes

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -22,10 +22,10 @@ import * as firebase from 'firebase';
 
 const ChatScreen = () => {
 
-    
     const navigation = useNavigation();
     const route = useRoute();
     const [input, setInput] = useState('');
+    const [messages, setMessages] = useState([]);
 
     const sendMessage = () => {
         Keyboard.dismiss();
@@ -97,6 +97,18 @@ const ChatScreen = () => {
         })
     }, [])
 
+    useLayoutEffect(() => db
+    .collection('chats')
+    .doc(route.params.id)
+    .collection('messages')
+    .orderBy('timestamp', 'desc')
+    .onSnapshot(snapshot => setMessages(
+        snapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data()
+        }))
+    )), [route])
+
   return (
     <SafeAreaView style={tw`flex-1 bg-white`}>
         <StatusBar style='light'/>
@@ -109,7 +121,37 @@ const ChatScreen = () => {
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 <>
                     <ScrollView>
-                        {/* <Text>{ route.params.chatName }</Text> */}
+                        { 
+                            messages.map(({id, data}) => (
+                                data.email === auth.currentUser.email
+                                ? (
+                                    <View key={id} style={styles.senderContainer}>
+                                        <Avatar 
+                                           style={tw`bg-red-500`} 
+                                        />
+
+                                        <Text
+                                            style={styles.senderText}
+                                        >
+                                            {data.message}
+                                        </Text>
+                                    </View>
+                                )
+                                : (
+                                    <View key={id} style={styles.receiverContainer}>
+                                        <Avatar 
+                                            
+                                        />
+
+                                    <Text
+                                        style={styles.receiverText}
+                                    >
+                                        {data.message}
+                                    </Text>
+                                </View>
+                                )
+                            )) 
+                        }
                     </ScrollView>
 
                     {/* This is the footer */}
@@ -154,7 +196,27 @@ const styles = StyleSheet.create({
         padding: 10,
         // color: 'grey',
         borderRadius: 30,
-    }
+    },
+    senderContainer: {
+        padding: 15,
+        backgroundColor: '#ECECEC',
+        alignSelf: 'flex-end',
+        borderRadius: 20,
+        marginRight: 15,
+        marginBottom: 20,
+        marginTop: 20,
+        maxWidth: '80%',
+        position: 'relative'
+    },
+    receiverContainer: {
+        padding: 15,
+        backgroundColor: '#3976f0',
+        alignSelf: 'flex-start',
+        borderRadius: 20,
+        margin: 15,
+        maxWidth: '80%',
+        position: 'relative'
+    },
 })
 
 export default ChatScreen
